bb29705bf39a50ad5cceb2d424f043d1fcc79719
Joeri Ridder <joeriridder@LIVE.NL>
2019-06-10 21:51:24 +0200

    Deleted more old prefabs + added debugger script

---
+++ Assets/Scripts/Debugger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Debugger : MonoBehaviour
+{
+    private Player player;
+
+    private void Awake()
+    {
+        player = FindObjectOfType<Player>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                player.Damage(1);
+            }
+        }
+    }
+}
